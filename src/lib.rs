155a2debabe0e212f9258fa177156196ce32bad6
Matteo Campanelli <matteo.campanelli@gmail.com>
2020-12-21 11:55:46 +0100

    Add tests for modeq+root

--- src/lib.rs
+++ src/lib.rs
@@ -17,6 +17,22 @@ use rand_core::OsRng;
 use curve25519_dalek::ristretto::CompressedRistretto;
 use curve25519_dalek::scalar::Scalar;
 
+
+
+/* 
+    NB: This implementation carries out set membership assuming a range test
+        has been done somewhere else
+*/
+pub struct SetMembership {
+
+}
+
+pub struct SetMemProof(R1CSProof);
+
+impl SetMembership {
+
+}
+
 pub struct Rerandomization {
     bp_gens: BulletproofGens,
     pc_gens: PedersenGens,
@@ -75,6 +91,102 @@ impl Rerandomization {
             .is_ok()
     }
 }
+
+
+#[cfg(test)]
+mod tests {
+    use cpsnarks_set::commitments::Commitment;
+    use cpsnarks_set::parameters::Parameters;
+    use cpsnarks_set::protocols::membership_simple::*;
+    use cpsnarks_set::protocols::membership_simple::transcript::*;
+
+
+    use accumulator::group::{Rsa2048};
+    use accumulator::{group::Group, AccumulatorWithoutHashToPrime};
+    use curve25519_dalek::{ristretto::RistrettoPoint, scalar::Scalar};
+    use merlin::Transcript;
+    use rand::thread_rng;
+    use rug::rand::RandState;
+    use rug::Integer;
+    use std::cell::RefCell;
+
+    const LARGE_PRIMES: [u64; 4] = [
+        553_525_575_239_331_913,
+        12_702_637_924_034_044_211,
+        378_373_571_372_703_133,
+        8_640_171_141_336_142_787,
+    ];
+
+
+    #[test]
+    fn test_proof() {
+        let params = Parameters::from_security_level(128).unwrap();
+        let mut rng1 = RandState::new();
+        rng1.seed(&Integer::from(13));
+        let mut rng2 = thread_rng();
+
+        let crs = Protocol::<
+            Rsa2048,
+            RistrettoPoint,
+        >::setup(&params, &mut rng1, &mut rng2)
+        .unwrap()
+        .crs;
+        let protocol = Protocol::<Rsa2048, RistrettoPoint>::from_crs(&crs);
+
+        let value = Integer::from(Integer::u_pow_u(
+            2,
+            (crs.parameters.hash_to_prime_bits) as u32,
+        )) - &Integer::from(245);
+        let randomness = Integer::from(5);
+        let commitment = protocol
+            .crs
+            .crs_modeq
+            .pedersen_commitment_parameters
+            .commit(&value, &randomness)
+            .unwrap();
+
+        let accum =
+            accumulator::Accumulator::<Rsa2048, Integer, AccumulatorWithoutHashToPrime>::empty();
+        let accum = accum.add(
+            &LARGE_PRIMES
+                .iter()
+                .skip(1)
+                .map(|p| Integer::from(*p))
+                .collect::<Vec<_>>(),
+        );
+
+        let accum = accum.add_with_proof(&[value.clone()]);
+        let acc = accum.0.value;
+        let w = accum.1.witness.0.value;
+        assert_eq!(Rsa2048::exp(&w, &value), acc);
+
+        let proof_transcript = RefCell::new(Transcript::new(b"membership"));
+        let mut verifier_channel = TranscriptVerifierChannel::new(&crs, &proof_transcript);
+        let statement = Statement {
+            c_e_q: commitment,
+            c_p: acc,
+        };
+        protocol
+            .prove(
+                &mut verifier_channel,
+                &mut rng1,
+                &mut rng2,
+                &statement,
+                &Witness {
+                    e: value,
+                    r_q: randomness,
+                    w,
+                },
+            )
+            .unwrap();
+        let proof = verifier_channel.proof().unwrap();
+        let verification_transcript = RefCell::new(Transcript::new(b"membership"));
+        let mut prover_channel =
+            TranscriptProverChannel::new(&crs, &verification_transcript, &proof);
+        protocol.verify(&mut prover_channel, &statement).unwrap();
+    }
+}
+
 
 /*
 mod tests {
